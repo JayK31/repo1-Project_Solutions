f39a07e4d1daee938d5c1a23ec735bee760635d6
JayK31 <jay.kaye31@gmail.com>
2014-02-21 00:00:43 -0500

    Initial commit

---
+++ pi_to_n.rb
@@ -0,0 +1,13 @@
+# Find PI to the Nth Digit
+# Enter a number and have the program generate PI up to that 
+# many decimal places. Keep a limit to how far the 
+# program will go
+
+
+
+def find_pi(n)
+	puts Math::PI
+	puts n
+end
+
+find_pi(7)
